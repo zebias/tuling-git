800c2804ab6230c3aac15f4f34540cbbfc8b2748
zebias <zebias@users.noreply.github.com>
2019-10-23 16:13:14 +0800

    Update UserControl.java

--- src/main/java/com/tuling/teach/controller/UserControl.java
+++ src/main/java/com/tuling/teach/controller/UserControl.java
@@ -7,4 +7,10 @@ public class UserControl {
     public void showName(){
         System.out.println("查看到了一个用户");
     }
+   /*
+       修改了用户名称
+   */
+    public void changeName(){
+        System.out.println("修改了一个用户名称");
+    }
 }
