d7662d0959fccc7f077f0fa2582d704028b3ee18
zebias <729976854@qq.com>
2019-10-24 17:21:17 +0800

    添加了web支持，项目编译成war包格式

---
+++ src/main/java/com/tuling/teach/ApplicationDev.java
@@ -0,0 +1,12 @@
+package com.tuling.teach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationDev {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationDev.class, args);
+    }
+}
--- src/main/java/com/tuling/teach/controller/UserControl.java
+++ src/main/java/com/tuling/teach/controller/UserControl.java
@@ -1,5 +1,10 @@
 package com.tuling.teach.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class UserControl {
     /*
         查看用户名称
@@ -13,5 +18,15 @@ public class UserControl {
     public void delName(){
         // 修改这个del功能
         System.out.print("删除了一个用户名称");
+    }
+
+    public void updateName() {
+        // update user
+    }
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String show(){
+        return "this is web page";
     }
 }
