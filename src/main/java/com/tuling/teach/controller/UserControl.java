9d59820281ff89934a693f23941f70bae40887b0
zebias <729976854@qq.com>
2019-10-25 09:47:29 +0800

    新增了无用代码，主要用于sha-1算法的使用

--- src/main/java/com/tuling/teach/controller/UserControl.java
+++ src/main/java/com/tuling/teach/controller/UserControl.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.security.MessageDigest;
+
 @Controller
 public class UserControl {
     /*
@@ -28,5 +30,46 @@ public class UserControl {
     @ResponseBody
     public String show(){
         return "this is web page";
+    }
+    /*
+        getSHA1
+     */
+    private String getSHA1(String ticket, String timestamp, String noncestr, String url){
+        // 拼接字符串
+        String str= "ticket="+ticket+"&noncestr="+
+        noncestr+"&timestamp="+
+                timestamp+"&url="+url;
+        // 打印内容
+        System.out.println("str: " + str);
+        // 对象
+        MessageDigest md;
+        try {
+            // 获取实例
+            md = MessageDigest.getInstance("SHA-1");
+            // 更新数据（字符串转byte数组）
+            md.update(str.getBytes());
+            // 获取byte数组
+            byte[] digest = md.digest();
+            // 创建一个字符串
+            StringBuffer hexstr = new StringBuffer();
+            // 复制为空字符串
+            String shaHex = "";
+            // 遍历数据
+            for (int i = 0; i < digest.length; i++) {
+                // 应该是转成16进制字符串
+                shaHex = Integer.toHexString(digest[i] & 0xFF);
+                // 如果每次遍历长度小于2就追加0
+                if (shaHex.length() < 2) {
+                    hexstr.append(0);
+                }
+                //把转好的添加到预先定义的字符串中，追加形式。
+                hexstr.append(shaHex);
+            }
+            // 返回字符串
+            return hexstr.toString();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
